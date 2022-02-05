d0179f087cee8cef1ca56888113975ac4a9caad4
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-02-05 12:50:21 +0100

    final version, auditlog.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const auditLog = require ("audit-log");
 const credentials = {secretUser:"user" , secretPassword:"password"}
 
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const mongoose = require ("mongoose");
 const https = require ('https');
 const http = require('http');
 const fs = require('fs');
@@ -14,6 +16,15 @@ var options = {
    key: fs.readFileSync('abels-key.pem'),
    cert: fs.readFileSync('abels-cert.pem')
 };
+
+auditLog.addTransport("mongoose", {connectionString: "mongodb://localhost:27017/myDatabase"});
+auditLog.addTransport("console");
+// descriptive parameters for your reading pleasure:
+auditLog.logEvent('user id or something', 'maybe script name or function', 'what just happened', 'the affected target name perhaps', 'target id', 'additional info, JSON, etc.');
+ 
+// a more realistic example:
+auditLog.logEvent(95049, 'AppServer', 'Shutdown', 'Production-3 Instance', 'ec2-255-255-255-255', 'Terminated from web console.');
+
 
 app.use(function (req, res, next) {
    res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
@@ -64,5 +75,5 @@ app.listen(PORT , ()=>{
 });
 
 https.createServer(options, app).listen(443, function(){
-      console.log('HTTPS LISTENING ON 433');
+      console.log('HTTPS LISTENING ON 443');
 })
